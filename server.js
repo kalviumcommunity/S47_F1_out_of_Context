b0fd755d279a2bc10e7af4704ee439ef1cfd5024
mysterious-egg <71733395+mysterious-egg@users.noreply.github.com>
2024-01-19 10:18:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
+
 const express = require('express');
 const app = express();
-const port = process.env.PUBLIC_PORT || 3000;
 
-// define the ping route with the response in JSON
-app.get('/', (req, res) => {
-    res.json({"message":"pong"},
-    {"message2":"Hey there,this is my first api"});
-  });
+app.get('/',(req, res) => {
+  res.send("Heyy There");
+});
 
-if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`🚀 server running on PORT: ${port}`);
-  });
-}
-
-module.exports = app;
+app.listen(5000,()=>{
+  console.log("port is working")
+})
